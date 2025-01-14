b068caf9b4c54f0f6f2782d22b0860f90c0fc920
Parthiv Makwana <75653580+parthiv11@users.noreply.github.com>
2025-01-14 19:51:58 +0000

    init version

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ]
+  },
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+};
+
+module.exports = nextConfig;
